d03f37a488cd07a3a2451a8dc3f5308e60d4f817
MirzaAyon <ayonmirza98@gmail.com>
2022-06-19 11:34:17 +0600

    basic function added

---
+++ function-basic.js
@@ -0,0 +1,22 @@
+// function add() {
+//     console.log('bro');
+// }
+// function minus() {
+//     console.log('bro');
+// }
+// function bro() {
+//     console.log('bro');
+// }
+//3 ta func er ek e kaj tai shudhu bro function take daak dibo
+
+function add() {
+    bro();
+}
+add(); //call korlam
+function minus() {
+    bro();
+}
+minus(); //call korlam
+function bro() {
+    console.log('bro');
+}
